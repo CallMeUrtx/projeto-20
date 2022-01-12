37322c56b4bf5b7dc00cdd8bfefbcb12757bc3e6
CallMeUrtx <92831509+CallMeUrtx@users.noreply.github.com>
2022-01-12 17:29:36 -0300

    Add files via upload

--- projeto 20/sketch.js
+++ projeto 20/sketch.js
@@ -16,20 +16,24 @@ function setup() {
   createCanvas(1000, 800)
   //crie os sprites de gato e rato aqui
 
-  bgImg.addimage("fundo", garden)
-  
+  bgImg.addImage('garden.png', garden)
+
   cat = createSprite(870, 600)
-  cat.addAnimation('gatoSentado', cat1)
-  cat.scate = 0.2
+  cat.addAnimation('cat1.png', cat1)
+  cat.scale = 0.2
+
+  mouse = createSprite(400, 300)
+  mouse.addAnimation('mouse1.png', mouse1)
+  mouse.scale = 0.2
 }
 
 function draw() {
   background(255)
   //Escreva a condição aqui para avaliar se o gato e o rato colidem
   if (cat.x - mouse.x < (cat.width - mouse.width) / 2) {
-    cat.velocityx = 0
-    cat.addAnimation('ultimaImagenGato', cat3)
-    cat.changeAnimation('ultimaImagenGato')
+    cat.velocityX = 0
+    cat.addAnimation('cat3.png', cat3)
+    cat.changeAnimation('cat4.png')
     cat.x = 300
     cat.scale = 0.2
   }
@@ -40,7 +44,8 @@ function keyPressed() {
   //Para mover e alterar a animação, escreva o código aqui
 
   if (keyCode === LEFT_ARROW) {
-    mouse.addAnimation('ratoProvocando')
+    mouse.addAnimation('mouse4.png', mouse2)
+    mouse.changeAnimation('mouse4.png')
     mouse.frameDelay = 25
 
     cat.velocityX = -5
